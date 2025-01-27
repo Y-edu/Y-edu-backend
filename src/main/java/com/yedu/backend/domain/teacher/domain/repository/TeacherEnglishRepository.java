fb250240b4051f7b8075c97312f40df311aa4720
yang <ywj9811@gmail.com>
2025-01-27 09:08:42 +0900

    feat : Teacher 관련 레포지토리 생성

---
+++ src/main/java/com/yedu/backend/domain/teacher/domain/repository/TeacherEnglishRepository.java
@@ -0,0 +1,7 @@
+package com.yedu.backend.domain.teacher.domain.repository;
+
+import com.yedu.backend.domain.teacher.domain.entity.TeacherEnglish;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherEnglishRepository extends JpaRepository<TeacherEnglish, Long> {
+}
