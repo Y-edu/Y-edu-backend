24a89f232813d4ad4fe80ced17c8886e9be865a7
yang <ywj9811@gmail.com>
2025-02-04 04:24:44 +0900

    feat : 신청서 엔티티 수정 및 레포지토리 구현

---
+++ src/main/java/com/yedu/backend/domain/parents/domain/repository/ApplicationFormRepository.java
@@ -0,0 +1,7 @@
+package com.yedu.backend.domain.parents.domain.repository;
+
+import com.yedu.backend.domain.parents.domain.entity.ApplicationForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationFormRepository extends JpaRepository<ApplicationForm, String> {
+}
