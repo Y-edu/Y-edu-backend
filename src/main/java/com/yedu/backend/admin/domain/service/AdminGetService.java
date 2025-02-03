697d5c1208d589d57a635824133426f52e3b2e3d
yang <ywj9811@gmail.com>
2025-02-04 06:44:29 +0900

    feat : admin 관련 Service 생성

---
+++ src/main/java/com/yedu/backend/admin/domain/service/AdminGetService.java
@@ -0,0 +1,35 @@
+package com.yedu.backend.admin.domain.service;
+
+import com.yedu.backend.domain.matching.domain.entity.ClassMatching;
+import com.yedu.backend.domain.parents.domain.entity.ApplicationForm;
+import com.yedu.backend.domain.parents.domain.repository.ApplicationFormRepository;
+import com.yedu.backend.domain.parents.domain.repository.ClassMatchingRepository;
+import com.yedu.backend.domain.parents.domain.repository.GoalRepository;
+import com.yedu.backend.domain.parents.domain.repository.ParentsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminGetService {
+    private final ApplicationFormRepository applicationFormRepository;
+    private final ClassMatchingRepository classMatchingRepository;
+    private final GoalRepository goalRepository;
+    private final ParentsRepository parentsRepository;
+
+    public List<ApplicationForm> allApplication() {
+        return applicationFormRepository.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+    }
+
+    public List<ClassMatching> allMatching(ApplicationForm applicationForm) {
+        return classMatchingRepository.findAllByApplicationForm(applicationForm);
+    }
+
+    public ApplicationForm byId(String applicationFormId) {
+        return applicationFormRepository.findById(applicationFormId)
+                .orElseThrow();
+    }
+}
---
+++ src/main/java/com/yedu/backend/domain/parents/domain/repository/ClassMatchingRepository.java
@@ -0,0 +1,11 @@
+package com.yedu.backend.domain.parents.domain.repository;
+
+import com.yedu.backend.domain.matching.domain.entity.ClassMatching;
+import com.yedu.backend.domain.parents.domain.entity.ApplicationForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClassMatchingRepository extends JpaRepository<ClassMatching, Long> {
+    List<ClassMatching> findAllByApplicationForm(ApplicationForm applicationForm);
+}
