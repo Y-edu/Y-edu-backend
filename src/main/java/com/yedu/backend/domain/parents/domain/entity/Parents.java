1c7a3f74f77026e98129b27f3a7f15874889f16e
yang <ywj9811@gmail.com>
2025-02-04 04:21:13 +0900

    fix : enum타입 변경

--- src/main/java/com/yedu/backend/domain/parents/domain/entity/Parents.java
+++ src/main/java/com/yedu/backend/domain/parents/domain/entity/Parents.java
@@ -1,9 +1,8 @@
 package com.yedu.backend.domain.parents.domain.entity;
 
+import com.yedu.backend.domain.teacher.domain.entity.constant.District;
 import com.yedu.backend.global.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,10 +16,18 @@ import lombok.NoArgsConstructor;
 public class Parents extends BaseEntity {
     @Id
     private long parentsId;
-    private int count;
+    @Column(nullable = false)
+    @Builder.Default
+    private int count = 1;
     private String kakaoName;
     @Column(nullable = false)
     private String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private District district;
+    @Column(nullable = false)
+    private String dong;
+    @Column(nullable = false)
     private int totalPayment;
     @Builder.Default
     private boolean marketingAgree = true;
