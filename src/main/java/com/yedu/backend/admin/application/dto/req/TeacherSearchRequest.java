85aef065d9bd62fbdb195e9f3571476ad1b3cb2c
yang <ywj9811@gmail.com>
2025-02-05 09:30:36 +0900

    fix : 요청시 지역 타입 변경

--- src/main/java/com/yedu/backend/admin/application/dto/req/TeacherSearchRequest.java
+++ src/main/java/com/yedu/backend/admin/application/dto/req/TeacherSearchRequest.java
@@ -1,12 +1,13 @@
 package com.yedu.backend.admin.application.dto.req;
 
 import com.yedu.backend.domain.parents.domain.entity.constant.ClassType;
+import com.yedu.backend.domain.teacher.domain.entity.constant.District;
 import com.yedu.backend.domain.teacher.domain.entity.constant.TeacherGender;
 
 import java.util.List;
 
 public record TeacherSearchRequest(
-        List<String> districts,
+        List<District> districts,
         List<ClassType> subjects,
         List<String> universities,
         List<TeacherGender> genders,
