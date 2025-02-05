3746904a7999130a737d76bfa46c4bb4e1a10330
yang <ywj9811@gmail.com>
2025-02-06 05:52:07 +0900

    feat : JWT 관련 설정 추가

---
+++ src/main/java/com/yedu/backend/global/config/security/jwt/auth/AuthDetailsService.java
@@ -0,0 +1,21 @@
+package com.yedu.backend.global.config.security.jwt.auth;
+
+import com.yedu.backend.admin.domain.entity.Admin;
+import com.yedu.backend.admin.domain.repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+    private final AdminRepository adminRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String id) {
+        Admin admin = adminRepository
+                .findById(Long.parseLong(id))
+                .orElseThrow();
+        return new AuthDetails(admin);
+    }
+}
---
+++ src/main/java/com/yedu/backend/global/config/security/jwt/filter/CustomAccessDeniedHandler.java
@@ -0,0 +1,33 @@
+package com.yedu.backend.global.config.security.jwt.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        log.error("AuthenticationEntryPoint : {}", request.getRequestURI());
+        objectMapper.writeValue(
+                response.getOutputStream(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+}
