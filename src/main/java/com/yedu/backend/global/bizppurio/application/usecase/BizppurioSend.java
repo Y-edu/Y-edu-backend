424c92fcd2aa33f72bbbfa8744bb6e5e04462798
yang <ywj9811@gmail.com>
2025-02-12 03:50:30 +0900

    fix : 알림톡 관련 수정

--- src/main/java/com/yedu/backend/global/bizppurio/application/usecase/BizppurioSend.java
+++ src/main/java/com/yedu/backend/global/bizppurio/application/usecase/BizppurioSend.java
@@ -6,6 +6,7 @@ import com.yedu.backend.global.bizppurio.application.dto.res.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -27,7 +28,7 @@ public class BizppurioSend {
 
     protected Mono<Void> sendMessageWithExceptionHandling(Supplier<CommonRequest> messageSupplier) {
         try {
-            log.info("알림톡 발송 : {}", messageSupplier.get().to());
+            log.info("알림톡 발송 : {} \n{}", messageSupplier.get().to(), messageSupplier.get().content().at());
             CommonRequest commonRequest = messageSupplier.get();
             String accessToken = bizppurioAuth.getAuth();
             String request = objectMapper.writeValueAsString(commonRequest);
@@ -37,6 +38,12 @@ public class BizppurioSend {
                     .headers(h -> h.setBearerAuth(accessToken))
                     .bodyValue(request)
                     .retrieve()
+                    .onStatus(HttpStatusCode::isError, response -> {
+                        log.error("알림톡 발송 실패 : {}", response.statusCode());
+                        return response.bodyToMono(String.class)
+                                .doOnNext(errorBody -> log.error("응답 본문 : {}", errorBody))
+                                .flatMap(error -> Mono.error(new IllegalArgumentException("비즈뿌리오 알림톡 전송 API 요청 실패")));
+                    })
                     .bodyToMono(MessageResponse.class)
                     .doOnNext(this::check)
                     .then();
@@ -48,10 +55,11 @@ public class BizppurioSend {
     }
 
     private void check(MessageResponse response) {
+        log.info("알림톡 전송에 결과. : {} {}", response.code(), response.description());
         if (response.code() != 1000) {
             log.error("전송실패 errorCode : {} errorMessage : {}", response.code(), response.description());
             throw new IllegalArgumentException(response.code() + ", " + response.description());
         }
-        log.info("알림톡 전송에 성공하였습니다. : {} {}", response.code(), response.description());
+        log.info("알림톡 전송에 성공하였습니다.");
     }
 }
