dd235fb93810d5ccdb1f7e5fe3aa107fda119a7a
yang <ywj9811@gmail.com>
2025-02-12 07:45:23 +0900

    feat : 알림톡 웹훅 결과 처리 로직 작성

--- src/main/java/com/yedu/backend/global/bizppurio/application/usecase/BizppurioSend.java
+++ src/main/java/com/yedu/backend/global/bizppurio/application/usecase/BizppurioSend.java
@@ -2,6 +2,7 @@ package com.yedu.backend.global.bizppurio.application.usecase;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yedu.backend.global.bizppurio.application.dto.req.CommonRequest;
+import com.yedu.backend.global.bizppurio.application.dto.req.MessageStatusRequest;
 import com.yedu.backend.global.bizppurio.application.dto.res.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -46,9 +47,9 @@ public class BizppurioSend {
                     })
                     .bodyToMono(MessageResponse.class)
                     .doOnSubscribe(subscription -> log.info("알림톡 요청 시작"))
-                    .doOnNext(response -> log.info("알림톡 전송 결과 : {} {}", response.code(), response.description()))
+                    .doOnNext(response -> log.info("알림톡 요청 결과 : {} {} {}", response.code(), response.description(), response.messagekey()))
                     .doOnError(error -> log.error("알림톡 요청 중 오류 발생: {}", error.getMessage()))
-                    .doOnSuccess(response -> log.info("알림톡 요청 성공"))
+                    .doOnSuccess(response -> log.info("알림톡 초기 요청 성공"))
                     .doOnNext(this::check)
                     .then();
         } catch (Exception ex) {
@@ -63,6 +64,15 @@ public class BizppurioSend {
             log.error("전송실패 errorCode : {} errorMessage : {}", response.code(), response.description());
             throw new IllegalArgumentException(response.code() + ", " + response.description());
         }
-        log.info("알림톡 전송에 성공하였습니다.");
+        log.info("알림톡 요청에 성공하였습니다.");
+    }
+
+    public void checkByWebHook(MessageStatusRequest request) {
+        if (request.RESULT().equals("7000")) {
+            log.info("{} 에 대한 알림톡 전송 완료", request.PHONE());
+            return;
+        }
+        log.error("{} 에 대한 알림톡 전송 실패, MessageKey : {} ResultCode : {}",  request.PHONE(), request.MSGID(), request.RESULT());
+        //todo : 디스코드 웹훅
     }
 }
