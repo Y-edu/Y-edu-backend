d23b4758b10bb823b04c8e82a9b1cd81be41d84d
yang <ywj9811@gmail.com>
2025-02-12 22:40:39 +0900

    feat : 신청건 저장시 금액 저장

--- src/main/java/com/yedu/backend/domain/parents/application/mapper/ParentsMapper.java
+++ src/main/java/com/yedu/backend/domain/parents/application/mapper/ParentsMapper.java
@@ -14,9 +14,13 @@ public class ParentsMapper {
     }
 
     public static ApplicationForm mapToApplicationForm(Parents parents, ApplicationFormRequest request) {
-        char count = (char) (parents.getCount() + 96);
+        // 가격 계산식 = 4주 기준 분 * 600
+
+        char total = (char) (parents.getCount() + 96);
+        int classCount = getClassCount(request.classCount());
+        int classTime = getClassTime(request.classTime());
         return ApplicationForm.builder()
-                .applicationFormId(District.fromString(request.district()) + String.valueOf(parents.getParentsId()) + count)
+                .applicationFormId(District.fromString(request.district()) + String.valueOf(parents.getParentsId()) + total)
                 .parents(parents)
                 .age(request.age())
                 .online(request.online())
@@ -29,7 +33,36 @@ public class ParentsMapper {
                 .classCount(request.classCount())
                 .classTime(request.classTime())
                 .source(request.source())
+                .pay(classTime*classCount*4*600)
                 .build();
+    }
+
+    private static int getClassTime(String time) {
+        int classTime = 0;
+        if (time.equals("50분"))
+            classTime = 50;
+        if (time.equals("60분"))
+            classTime = 60;
+        if (time.equals("75분"))
+            classTime = 75;
+        if (time.equals("100분"))
+            classTime = 100;
+        if (time.equals("120분"))
+            classTime = 120;
+        return classTime;
+    }
+
+    private static int getClassCount(String count) {
+        int classCount = 0;
+        if (count.equals("주 1회"))
+            classCount = 1;
+        else if (count.equals("주 2회"))
+            classCount = 2;
+        else if (count.equals("주 3회"))
+            classCount = 3;
+        else if (count.equals("주 4회"))
+            classCount = 4;
+        return classCount;
     }
 
     public static Goal mapToGoal(ApplicationForm applicationForm, String classGoal) {
