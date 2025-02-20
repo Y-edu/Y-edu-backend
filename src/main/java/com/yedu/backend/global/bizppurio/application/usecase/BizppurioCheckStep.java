a9d5263c665b7897d197bc76f604386f19dc2b6e
yang <ywj9811@gmail.com>
2025-02-20 18:47:25 +0900

    fix : 알림톡 순서 보장하도록 수정

---
+++ src/main/java/com/yedu/backend/global/bizppurio/application/usecase/BizppurioCheckStep.java
@@ -0,0 +1,31 @@
+package com.yedu.backend.global.bizppurio.application.usecase;
+
+import com.yedu.backend.global.bizppurio.application.dto.req.MessageStatusRequest;
+import com.yedu.backend.global.config.redis.RedisRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class BizppurioCheckStep {
+    private final RedisRepository redisRepository;
+    private final BizppurioTeacherMessage teacherMessage;
+
+    private static final String COUNSEL = "COUNSEL";
+
+    public void checkNextStep(MessageStatusRequest request) {
+        getCacheValue(request.REFKEY()).ifPresent(stepValue -> processNextStep(stepValue, request.PHONE()));
+    }
+
+    private Optional<String> getCacheValue(String refKey) {
+        return redisRepository.getValues(refKey);
+    }
+
+    private void processNextStep(String stepValue, String phone) {
+        if (COUNSEL.equals(stepValue)) {
+            teacherMessage.photoSubmit(phone);
+        }
+    }
+}
