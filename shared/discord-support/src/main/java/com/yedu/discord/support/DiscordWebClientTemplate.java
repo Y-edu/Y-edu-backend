628bb9ebe127acf6506134f982e7f3d2db739e88
csbiy <katd6@naver.com>
2025-04-10 17:19:36 +0900

    chore : discord template 에러 로깅 상세화

--- shared/discord-support/src/main/java/com/yedu/discord/support/DiscordWebClientTemplate.java
+++ shared/discord-support/src/main/java/com/yedu/discord/support/DiscordWebClientTemplate.java
@@ -1,5 +1,7 @@
 package com.yedu.discord.support;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yedu.discord.support.dto.req.DiscordWebhookRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,16 +18,27 @@ public class DiscordWebClientTemplate {
 
   private final DiscordWebClientProperties properties;
 
-  public void sendWebhook(DiscordWebhookType webhookType, DiscordWebhookRequest request){
+  private final ObjectMapper objectMapper;
+
+  public void sendWebhook(DiscordWebhookType webhookType, DiscordWebhookRequest request) {
+    String url = properties.resolveUrl(webhookType);
+
     discordWebClient.post()
-        .uri(properties.resolveUrl(webhookType))
+        .uri(url)
         .accept(MediaType.APPLICATION_JSON)
         .contentType(MediaType.APPLICATION_JSON)
         .bodyValue(request)
         .retrieve()
         .bodyToMono(Void.class)
         .doOnSuccess(response -> log.info("Discord 웹훅 전송 성공"))
-        .doOnError(error -> log.error("Discord 웹훅 전송 실패: {}", error.getMessage()))
+        .doOnError(error -> {
+          try {
+            String jsonRequest = objectMapper.writeValueAsString(request);
+            log.error("Discord 웹훅 전송 실패: {} / 요청 경로 : {} /원본 요청: {}", error.getMessage(), url, jsonRequest);
+          } catch (JsonProcessingException e) {
+            log.error("요청 직렬화 실패: {}", e.getMessage(), e);
+          }
+        })
         .subscribe();
   }
 
