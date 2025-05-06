c4e0847199315df54062a099906d8669566de46d
csbiy <katd6@naver.com>
2025-05-06 22:06:58 +0900

    hotfix

--- consumer/src/main/resources/db/migration/V1_202505062124__create_notification_table.sql
+++ consumer/src/main/resources/db/migration/V1_202505062124__create_notification_table.sql
@@ -1,7 +1,7 @@
 create table push.notification (
       id bigint not null auto_increment,
       consumed_at datetime(6),
-      content varchar(255),
+      content varchar(5000),
       push_key varchar(255),
       receiver_phone_number varchar(255),
       push_type enum ('BIZPURRIO_KAKAO_ALARMTALK'),
