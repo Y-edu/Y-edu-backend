aa1cfb7fce1d19cd7dbaab3ca70e79e0b3e215f3
csbiy <katd6@naver.com>
2025-05-06 22:02:25 +0900

    hotfix

--- shared/rabbitmq-support/src/main/java/com/yedu/rabbitmq/support/RabbitMqConfig.java
+++ shared/rabbitmq-support/src/main/java/com/yedu/rabbitmq/support/RabbitMqConfig.java
@@ -1,11 +1,7 @@
 package com.yedu.rabbitmq.support;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -26,20 +22,21 @@ public class RabbitMqConfig {
   }
 
   @Bean
+  public DirectExchange deadLetterExchange() {
+    return new DirectExchange(properties.exchange() + ".dlx");
+  }
+
+  @Bean
   public Queue parentQueue() {
-    return QueueBuilder.durable(properties.parent().queue()).build();
-  }
-
-  @Bean
-  public Queue teacherQueue() {
-    return QueueBuilder.durable(properties.teacher().queue()).build();
-  }
-
-  @Bean
-  public Queue systemQueue() {
-    return QueueBuilder.durable(properties.system().queue())
-        .withArgument("x-max-priority", 10)
+    return QueueBuilder.durable(properties.parent().queue())
+        .withArgument("x-dead-letter-exchange", properties.exchange() + ".dlx")
+        .withArgument("x-dead-letter-routing-key", properties.parent().queue() + ".dlq")
         .build();
+  }
+
+  @Bean
+  public Queue parentDlq() {
+    return QueueBuilder.durable(properties.parent().queue() + ".dlq").build();
   }
 
   @Bean
@@ -48,13 +45,62 @@ public class RabbitMqConfig {
   }
 
   @Bean
+  public Binding parentDlqBinding(Queue parentDlq, DirectExchange deadLetterExchange) {
+    return BindingBuilder.bind(parentDlq)
+        .to(deadLetterExchange)
+        .with(properties.parent().queue() + ".dlq");
+  }
+
+  // teacher 큐 및 DLQ
+  @Bean
+  public Queue teacherQueue() {
+    return QueueBuilder.durable(properties.teacher().queue())
+        .withArgument("x-dead-letter-exchange", properties.exchange() + ".dlx")
+        .withArgument("x-dead-letter-routing-key", properties.teacher().queue() + ".dlq")
+        .build();
+  }
+
+  @Bean
+  public Queue teacherDlq() {
+    return QueueBuilder.durable(properties.teacher().queue() + ".dlq").build();
+  }
+
+  @Bean
   public Binding teacherBinding(Queue teacherQueue, DirectExchange exchange) {
     return BindingBuilder.bind(teacherQueue).to(exchange).with(properties.teacher().routingKey());
   }
 
   @Bean
+  public Binding teacherDlqBinding(Queue teacherDlq, DirectExchange deadLetterExchange) {
+    return BindingBuilder.bind(teacherDlq)
+        .to(deadLetterExchange)
+        .with(properties.teacher().queue() + ".dlq");
+  }
+
+  @Bean
+  public Queue systemQueue() {
+    return QueueBuilder.durable(properties.system().queue())
+        .withArgument("x-max-priority", 10)
+        .withArgument("x-dead-letter-exchange", properties.exchange() + ".dlx")
+        .withArgument("x-dead-letter-routing-key", properties.system().queue() + ".dlq")
+        .build();
+  }
+
+  @Bean
+  public Queue systemDlq() {
+    return QueueBuilder.durable(properties.system().queue() + ".dlq").build();
+  }
+
+  @Bean
   public Binding systemBinding(Queue systemQueue, DirectExchange exchange) {
     return BindingBuilder.bind(systemQueue).to(exchange).with(properties.system().routingKey());
+  }
+
+  @Bean
+  public Binding systemDlqBinding(Queue systemDlq, DirectExchange deadLetterExchange) {
+    return BindingBuilder.bind(systemDlq)
+        .to(deadLetterExchange)
+        .with(properties.system().queue() + ".dlq");
   }
 
   @Bean
