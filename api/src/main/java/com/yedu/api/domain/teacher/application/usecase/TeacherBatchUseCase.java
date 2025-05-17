2fcfbd91f256048f13f6fd5cd180c73f068e38c9
csbiy <katd6@naver.com>
2025-05-17 14:20:59 +0900

    fix

--- api/src/main/java/com/yedu/api/domain/teacher/application/usecase/TeacherBatchUseCase.java
+++ api/src/main/java/com/yedu/api/domain/teacher/application/usecase/TeacherBatchUseCase.java
@@ -27,7 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
-@Transactional(readOnly = true)
+@Transactional(readOnly = false)
 @Slf4j
 public class TeacherBatchUseCase {
 
