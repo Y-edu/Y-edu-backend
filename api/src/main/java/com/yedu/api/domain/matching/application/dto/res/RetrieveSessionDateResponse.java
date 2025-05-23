e069002302089598b6f5d25349594f5cfebfa9cc
csbiy <katd6@naver.com>
2025-05-23 17:23:43 +0900

    feat : teacherId 추가

--- api/src/main/java/com/yedu/api/domain/matching/application/dto/res/RetrieveSessionDateResponse.java
+++ api/src/main/java/com/yedu/api/domain/matching/application/dto/res/RetrieveSessionDateResponse.java
@@ -2,9 +2,9 @@ package com.yedu.api.domain.matching.application.dto.res;
 
 import java.time.LocalDate;
 
-public record RetrieveSessionDateResponse(LocalDate sessionDate, Boolean isComplete) {
+public record RetrieveSessionDateResponse(LocalDate sessionDate, Boolean isComplete, Long teacherId) {
 
   public static RetrieveSessionDateResponse empty() {
-    return new RetrieveSessionDateResponse(null, false);
+    return new RetrieveSessionDateResponse(null, false, null);
   }
 }
