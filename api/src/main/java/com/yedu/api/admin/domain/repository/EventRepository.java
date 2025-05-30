e8f5471dc55ed46d1bbe85ac543149fe7ddd9310
csbiy <katd6@naver.com>
2025-05-30 17:51:21 +0900

    feat : mutation 추가 , classMinute 추가

--- api/src/main/java/com/yedu/api/admin/domain/repository/EventRepository.java
+++
@@ -1,6 +0,0 @@
-package com.yedu.api.admin.domain.repository;
-
-import com.yedu.api.admin.domain.entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {}
