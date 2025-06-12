4cf9fe259cd91cf0b974fcda4de02d4f966f7ba3
csbiy <katd6@naver.com>
2025-06-12 22:17:49 +0900

    fix : 정규일정 변경했는데 변경 시작 날짜가 적용 안되는 문제

--- api/src/main/java/com/yedu/api/domain/matching/domain/entity/ClassSchedule.java
+++ api/src/main/java/com/yedu/api/domain/matching/domain/entity/ClassSchedule.java
@@ -58,15 +58,12 @@ public class ClassSchedule extends BaseEntity {
       Map<LocalDate, ClassSession> existingSessionMap,
       LocalDate changeStartDate) {
     LocalDate classStartDate =
-        Optional.ofNullable(classManagement.getFirstDay())
-            .map(
-                firstDay -> {
-                  if (firstDay.isBefore(today)) {
-                    return today;
-                  }
-                  return firstDay;
-                })
-            .orElse(Optional.ofNullable(changeStartDate).orElse(today));
+        Optional.ofNullable(changeStartDate)
+            .or(() ->
+                Optional.ofNullable(classManagement.getFirstDay())
+                    .map(firstDay -> firstDay.isBefore(today) ? today : firstDay)
+            )
+            .orElse(today);
     LocalDate lastDay = today.plusMonths(2).with(TemporalAdjusters.lastDayOfMonth());
 
     return Stream.iterate(classStartDate, date -> !date.isAfter(lastDay), date -> date.plusDays(1))
